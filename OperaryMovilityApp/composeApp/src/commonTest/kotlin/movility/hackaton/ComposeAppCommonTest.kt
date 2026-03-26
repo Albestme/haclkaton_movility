74aa8f5e927584100612f83547858ab65e736808
Albestme <albertbadilu@gmail.com>
2026-03-26 13:22:18 +0100

    created app project

---
+++ OperaryMovilityApp/composeApp/src/commonTest/kotlin/movility/hackaton/ComposeAppCommonTest.kt
@@ -0,0 +1,12 @@
+package movility.hackaton
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class ComposeAppCommonTest {
+
+    @Test
+    fun example() {
+        assertEquals(3, 1 + 2)
+    }
+}
